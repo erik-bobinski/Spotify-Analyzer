e2a0430e87e59a9411354ed8971414e04acbbda5
Ryan Connolly <95229435+ryanvconnolly@users.noreply.github.com>
2024-12-09 22:42:35 -0500

    Update DataProcessor.py

--- DataProcessor.py
+++ DataProcessor.py
@@ -1,6 +1,6 @@
 # Author: Ryan Connolly
 # December 10, 2024
-# Description: This modules handles the Spotify data upload and preprocesses features to be suitable for Singular Value Decomposition. 
+# Description: This modules defines the DataProcessor class that handles the Spotify data upload and preprocesses features to be suitable for cosine similarity calculations. 
 
 
 import pandas as pd 
@@ -17,20 +17,38 @@ class DataProcessor:
         
     def loadData(self):
         '''
-        load dataset from csv into pandas data frame
+        Load dataset from csv into pandas data frame
         returns: pd.DataFrame = the loaded dataset
         '''
         self.data = pd.read_csv(self.file_path)  
+        
+        if self.data is None:
+            raise FileNotFoundError("Data not found.")
         return self.data
     
+    def clean(self):
+        '''
+        Cleans dataset to handle missing values and duplicates
+        returns: pd.DataFrame = cleaned dataset
+        '''
+        self.data = self.data.drop_duplicates()  # remove duplicate entries
+        
+        important = ['name', 'id', 'artists']      # important columns for analysis
+        self.data = self.data.dropna(subset=important)            # drop rows with missing critical values
+        
+        cols = ['valence', 'danceability', 'energy', 'tempo', 'acousticness']
+        self.data[cols] = self.data[cols].apply(lambda x: x.fillna(x.mean()), axis=0)  # fills in missing values in feature columns with column mean
+        
+        
     def preprocessData(self):
         '''
-        normalizing relevant features for similarity calculations
+        Normalizing relevant features for similarity calculations
         
         returns: pd.DataFrame = preprocessed dataset with normalized features
         '''
+        
         # features to normalize for similarity
-        features = ['valence', 'danceability', 'energy', 'tempo', 'acoustics']
+        features = ['valence', 'danceability', 'energy', 'tempo', 'acousticness']
         
         # normalize each feature to have mean 0 and std 1.
         self.data[features] = self.data[features].apply(lambda x: x - x.mean() / x.std())
